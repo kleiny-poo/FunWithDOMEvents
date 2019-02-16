4a89efa8427a64b241c95a533f75fa236b57ce07
Klein <klein.simon@csedge.org>
2019-02-16 14:56:18 -0500

    Chnages
    Problems

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
-//let ch1=document.getElementById("challenge-one");
+let ch1=document.getElementById("challenge-one");
 
-//ch1.addEventListener("dblclick", disappear);
+ch1.addEventListener("dblclick", disappear);
 
 
-//function disappear() {
-//	ch1.fill: transparent;
-//}
+function disappear() {
+	ch1.fill: transparent;
+}
 
 
 let playBtn= document.getElementById("play-button");
 
 playBtn.addEventListener("mouseover", whenHovered);
 playBtn.addEventListener("mouseout", whenDoneHovering);
-
 function whenHovered() {
-	playBtn.style.background-color = "pink";
+	playBtn.style.color = "pink";
 }
 
 function whenDoneHovering() {
-	playBtn.style.background-color = "lightgreen";
+	playBtn.style.color = "lightgreen";
 }
+
+
+
