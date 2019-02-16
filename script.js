89c834f750e29397febff3bb6e21126de875344a
Klein <klein.simon@csedge.org>
2019-02-16 14:54:39 -0500

    Changes
    Changes

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
-let playBtn=document.getElementById("play-button");
+//let ch1=document.getElementById("challenge-one");
 
-playBtn.addEventListener("click", move);
+//ch1.addEventListener("dblclick", disappear);
 
 
-function move() {
-	playBtn.style.top=Math.random()*100 + "%";
-	playBtn.style.left=Math.random()*100 + "%";
+//function disappear() {
+//	ch1.fill: transparent;
+//}
+
+
+let playBtn= document.getElementById("play-button");
+
+playBtn.addEventListener("mouseover", whenHovered);
+playBtn.addEventListener("mouseout", whenDoneHovering);
+
+function whenHovered() {
+	playBtn.style.background-color = "pink";
+}
+
+function whenDoneHovering() {
+	playBtn.style.background-color = "lightgreen";
 }
